2e43d5a3f20b096a854f57c2fc8c17fd4ef22dd3
Roberto Filippone <rfilippone@gmail.com>
2013-11-26 16:29:09 +0100

    Removed client side alert when sending a request

--- src/main/java/org/gradle/gwt/shared/api/HMACDispatcher.java
+++ src/main/java/org/gradle/gwt/shared/api/HMACDispatcher.java
@@ -21,92 +21,84 @@ import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.DateTimeFormat.PredefinedFormat;
-import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 /**
  * @author FirstName LastName
- *
+ * 
  */
 @Singleton
 public class HMACDispatcher implements Dispatcher {
-	public static HMACDispatcher INSTANCE;
-	
-	Model model;
-	SJCL crypto;
-	CodecBase64 base64;
-	CodecUtf8String utf8;
-	CodecHex hex;
-	HashSha256 sha256;
-	
-	@Inject
-	protected HMACDispatcher(Model model) {
-		this.model = model;
-		INSTANCE = this;
-		crypto = SJCL.sjcl();
-		base64 = crypto.codec().base64();
-		utf8 = crypto.codec().utf8String();
-		hex = crypto.codec().hex();
-		sha256 = crypto.hash().sha256();
-	}
-		
-	private String hash(String text) {
-		return hex.fromBits(sha256.hash(text));
-	}
-	
-	public native final BitArrayType _hmac(SJCL crypto, BitArrayType key, BitArrayType text) /*-{
-		var mac = new crypto.misc.hmac(key);
-		return mac.mac(text);
-	}-*/;
+    public static HMACDispatcher INSTANCE;
 
-	private BitArrayType hmac(BitArrayType key, BitArrayType text)
-	{
-		return _hmac(crypto, key, text);
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.fusesource.restygwt.client.Dispatcher#send(org.fusesource.restygwt.client.Method, com.google.gwt.http.client.RequestBuilder)
-	 */
-	@Override
-	public Request send(Method method, RequestBuilder builder) throws RequestException {
+    Model model;
+    SJCL crypto;
+    CodecBase64 base64;
+    CodecUtf8String utf8;
+    CodecHex hex;
+    HashSha256 sha256;
 
-		try {					
-			String content = builder.getRequestData() != null ?  builder.getRequestData() : "";
+    @Inject
+    protected HMACDispatcher(Model model) {
+        this.model = model;
+        INSTANCE = this;
+        crypto = SJCL.sjcl();
+        base64 = crypto.codec().base64();
+        utf8 = crypto.codec().utf8String();
+        hex = crypto.codec().hex();
+        sha256 = crypto.hash().sha256();
+    }
 
-			DateTimeFormat fmt = DateTimeFormat.getFormat(PredefinedFormat.ISO_8601);
-		    String date = fmt.format(new Date());
-		    
-		    builder.setHeader(Resource.HEADER_ACCEPT, Resource.CONTENT_TYPE_JSON);
-		    
-		    String signedHeaders = "accept;x-hmac-date";
-			String canonicalRequest = builder.getHTTPMethod() + "\n" +			
-					builder.getUrl() + "\n" +
-					Resource.CONTENT_TYPE_JSON + "\n" +
-					date + "\n" +
-					signedHeaders + "\n" +
-					hash(content) + "\n";
-			
-			Window.alert(canonicalRequest);
-			
-			String hashedCanonicalRequest = hash(canonicalRequest);
+    private String hash(String text) {
+        return hex.fromBits(sha256.hash(text));
+    }
 
-			String stringToSign = "HMAC-SHA256\n" +
-					date + "\n" +
-					model.user + "\n" +
-					hashedCanonicalRequest;
-			
-			String signature = hex.fromBits(hmac(hmac(hmac(hex.toBits(model.ssk), utf8.toBits(date)), utf8.toBits(model.user)), utf8.toBits(stringToSign)));
-						
-			String authorization = "REST-HMAC-SHA256_1.0.0/" + model.user + "/" + signedHeaders + "/" + signature;			
-			
-			builder.setHeader("X-HMAC-Date", date);
-			builder.setHeader("X-HMAC-Auth", authorization);
-			
-			return builder.send();
-		} catch (Exception e) {
-			throw new RequestException("Unable to sign request", e);
-		}	
-	}
+    public native final BitArrayType _hmac(SJCL crypto, BitArrayType key, BitArrayType text) /*-{
+                                                                                             var mac = new crypto.misc.hmac(key);
+                                                                                             return mac.mac(text);
+                                                                                             }-*/;
+
+    private BitArrayType hmac(BitArrayType key, BitArrayType text) {
+        return _hmac(crypto, key, text);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.fusesource.restygwt.client.Dispatcher#send(org.fusesource.restygwt.client.Method, com.google.gwt.http.client.RequestBuilder)
+     */
+    @Override
+    public Request send(Method method, RequestBuilder builder) throws RequestException {
+
+        try {
+            String content = builder.getRequestData() != null ? builder.getRequestData() : "";
+
+            DateTimeFormat fmt = DateTimeFormat.getFormat(PredefinedFormat.ISO_8601);
+            String date = fmt.format(new Date());
+
+            builder.setHeader(Resource.HEADER_ACCEPT, Resource.CONTENT_TYPE_JSON);
+
+            String signedHeaders = "accept;x-hmac-date";
+            String canonicalRequest = builder.getHTTPMethod() + "\n" + builder.getUrl() + "\n" + Resource.CONTENT_TYPE_JSON + "\n" + date + "\n"
+                    + signedHeaders + "\n" + hash(content) + "\n";
+
+            String hashedCanonicalRequest = hash(canonicalRequest);
+
+            String stringToSign = "HMAC-SHA256\n" + date + "\n" + model.user + "\n" + hashedCanonicalRequest;
+
+            String signature = hex.fromBits(hmac(hmac(hmac(hex.toBits(model.ssk), utf8.toBits(date)), utf8.toBits(model.user)),
+                    utf8.toBits(stringToSign)));
+
+            String authorization = "REST-HMAC-SHA256_1.0.0/" + model.user + "/" + signedHeaders + "/" + signature;
+
+            builder.setHeader("X-HMAC-Date", date);
+            builder.setHeader("X-HMAC-Auth", authorization);
+
+            return builder.send();
+        } catch (Exception e) {
+            throw new RequestException("Unable to sign request", e);
+        }
+    }
 
 }
